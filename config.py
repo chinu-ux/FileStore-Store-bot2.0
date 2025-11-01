c07ea2ca94e2595f30e338b6f2486c7f61420019
chinu-ux <bilashinikhatua@gmail.com>
2025-11-01 12:58:45 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 123456              # from my.telegram.org
+API_HASH = "your_api_hash"
+BOT_TOKEN = "123456:ABC-DEF..." 
+OWNER_ID = 123456789         # your telegram user id (admin)
+CHANNEL_ID = -1001234567890  # private channel id (negative big)
+SESSION_NAME = "file_store_bot"
+BASE_URL = "https://t.me/YourBot?start="  # used for public short links
