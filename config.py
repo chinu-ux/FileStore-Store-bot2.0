f4a6075973e1ae22343efd99700c110657a2f972
chinu-ux <bilashinikhatua@gmail.com>
2025-11-01 13:16:02 +0530

    Update BASE_URL for the bot

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BOT_TOKEN = "8222645012:AAEQMNK31oa5hDo_9OEStfNL7FMBdZMkUFM"
 OWNER_ID = 7681308594         # your telegram user id (admin)
 CHANNEL_ID = -1003292247930  # private channel id (negative big)
 SESSION_NAME = "file_store_bot"
-BASE_URL = "https://t.me/YourBot?start="  # used for public short links
+BASE_URL = "https://t.me/Cornsebot?start="  # used for public short links
