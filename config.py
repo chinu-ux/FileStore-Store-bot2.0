eaf8ae5e96fec3e019d547ccf640b2d047d35f69
chinu-ux <bilashinikhatua@gmail.com>
2025-11-01 13:14:10 +0530

    Update API credentials and channel IDs

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456              # from my.telegram.org
-API_HASH = "your_api_hash"
-BOT_TOKEN = "123456:ABC-DEF..." 
-OWNER_ID = 123456789         # your telegram user id (admin)
-CHANNEL_ID = -1001234567890  # private channel id (negative big)
+API_ID = 29167872              # from my.telegram.org
+API_HASH = "f26b4e46f6d6e5ad36f480e73357425d"
+BOT_TOKEN = "8222645012:AAEQMNK31oa5hDo_9OEStfNL7FMBdZMkUFM" 
+OWNER_ID = 7681308594         # your telegram user id (admin)
+CHANNEL_ID = -1003292247930  # private channel id (negative big)
 SESSION_NAME = "file_store_bot"
 BASE_URL = "https://t.me/YourBot?start="  # used for public short links
